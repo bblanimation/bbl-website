06f4560cc24bcfa351d281e319f7f1848ae078ea
Christopher Gearhart <cgear13@gmail.com>
2017-03-02 13:27:36 -0500

    Added onClick to links for tracking important events with Google Analytics

--- scripts/insert-refactored-code.js
+++ scripts/insert-refactored-code.js
@@ -11,10 +11,10 @@ DomReady.ready(function() {
 
     var social = document.createElement("ul");
     social.className = "footer-social-icons"
-    social.innerHTML += '<li><a href="https://www.facebook.com/bblanimation/" target="_blank"><img class="social-icons" src="/images/social/facebook.png" alt="Facebook"></a></li>';
-    social.innerHTML += '<li><a href="https://plus.google.com/+bigbudlego/" target="_blank"><img class="social-icons" src="/images/social/google+.png" alt="Google+"></a></li>';
-    social.innerHTML += '<li><a href="https://www.youtube.com/c/bigbudlego" target="_blank"><img class="social-icons" src="/images/social/youtube.png" alt="YouTube"></a></li>';
-    social.innerHTML += '<li><a href="https://www.linkedin.com/in/christopher-gearhart-16b902a3" target="_blank"><img class="social-icons" src="/images/social/linkedin.png" alt="LinkedIn"></a></li>';
+    social.innerHTML += '<li><a href="https://www.facebook.com/bblanimation/" onClick="_gaq.push([\'_trackEvent\', \'social_links\', \'click_link\', \'facebook\', \'0\']);" target="_blank"><img class="social-icons" src="/images/social/facebook.png" alt="Facebook"></a></li>';
+    social.innerHTML += '<li><a href="https://plus.google.com/+bigbudlego/" onClick="_gaq.push([\'_trackEvent\', \'social_links\', \'click_link\', \'google+\', \'0\']);" target="_blank"><img class="social-icons" src="/images/social/google+.png" alt="Google+"></a></li>';
+    social.innerHTML += '<li><a href="https://www.youtube.com/c/bigbudlego" onClick="_gaq.push([\'_trackEvent\', \'social_links\', \'click_link\', \'youtube\', \'0\']);" target="_blank"><img class="social-icons" src="/images/social/youtube.png" alt="YouTube"></a></li>';
+    social.innerHTML += '<li><a href="https://www.linkedin.com/in/christopher-gearhart-16b902a3" onClick="_gaq.push([\'_trackEvent\', \'social_links\', \'click_link\', \'linkedin\', \'0\']);" target="_blank"><img class="social-icons" src="/images/social/linkedin.png" alt="LinkedIn"></a></li>';
     p.append(social);
 
     footer.appendChild(p);
